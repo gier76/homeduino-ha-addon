a79e52f9ece2bf1ffb340f116913a052a744c6ec
gier76 <test@example.com>
2026-03-15 19:17:30 +0100

    Bump version to 3.4.8 and optimize serial port forcing for Hyper-V

--- homeduino_bridge/index.js
+++ homeduino_bridge/index.js
@@ -44,7 +44,8 @@ class Homeduino extends EventEmitter {
     const portOptions = {
       path: this.portPath,
       baudRate: this.baudRate,
-      autoOpen: false
+      autoOpen: false,
+      lock: false // Sometimes helps with emulated ports
     };
 
     this.serial = new SerialPort(portOptions);
@@ -53,14 +54,17 @@ class Homeduino extends EventEmitter {
       if (err) {
         logToFile(`Serial Open Error: ${err.message}`);
         
-        // If it's a baud rate error, some virtual ports are already "configured" by the host
-        // We try to emit 'open' anyway if the error is just about the baud rate setting
-        if (err.message.includes('baud rate') || err.message.includes('I/O error')) {
-          logToFile(`Ignoring baud rate error on virtual port, attempting to continue...`);
+        if (err.message.includes('baud rate') || err.message.includes('I/O error') || err.message.includes('Operation not permitted')) {
+          logToFile(`Hyper-V Virtual Port detected. Forcing connection...`);
+          // Even if open() returned an error due to baud rate setting, 
+          // the fd might be valid. We try to continue.
           this.connected = true;
           this.emit('connected');
-          // Manual trigger of open logic since .open() failed
-          this.write('RF receive 0');
+          
+          // Small delay before sending initial command
+          setTimeout(() => {
+            this.write('RF receive 0');
+          }, 1000);
         } else {
           this.emit('error', err);
         }
@@ -323,7 +327,7 @@ io.on('connection', (socket) => {
       name: name || `Homeduino ${protocol} ${uid.split('_').slice(-2).join(':')}`,
       model: protocol,
       manufacturer: "Homeduino Bridge",
-      sw_version: "3.4.7"
+      sw_version: "3.4.8"
     };
 
     if (type === 'switch' || values.state !== undefined) {
