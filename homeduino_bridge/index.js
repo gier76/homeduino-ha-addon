cc6d27ab43f3e48ea5d95d29df9dcaa587efacb2
gier76 <test@example.com>
2026-03-17 21:48:56 +0100

    Bump version to 3.7.4 and fix inconsistency

--- homeduino_bridge/index.js
+++ homeduino_bridge/index.js
@@ -327,7 +327,7 @@ io.on('connection', (socket) => {
       name: name || `Homeduino ${protocol} ${uid.split('_').slice(-2).join(':')}`,
       model: protocol,
       manufacturer: "Homeduino Bridge",
-      sw_version: "3.4.9"
+      sw_version: "3.7.4"
     };
 
     if (type === 'switch' || values.state !== undefined) {
